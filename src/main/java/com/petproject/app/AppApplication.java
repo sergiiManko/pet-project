eb09277c51e274a6da2274c7ebaa3624adaecba5
Sergii Manko <sergii.manko@gmail.com>
2018-09-01 20:30:39 +0300

    Add dependency to project + add initial properties

--- src/main/java/com/petproject/app/AppApplication.java
+++ src/main/java/com/petproject/app/AppApplication.java
@@ -2,11 +2,25 @@ package com.petproject.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.convert.threeten.Jsr310JpaConverters;
+
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
 
 @SpringBootApplication
+@EntityScan(basePackageClasses = {
+        AppApplication.class,
+        Jsr310JpaConverters.class
+})
 public class AppApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(AppApplication.class, args);
+    }
+
+    @PostConstruct
+    void init() {
+        TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
     }
 }
